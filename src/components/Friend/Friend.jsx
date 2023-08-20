3a94db16eca853e32ef196abcd0dd80c6948232a
biswasbittu <biswasbittu7415@yahoo.com>
2023-08-20 13:05:29 +0530

    data uploded done

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -1,12 +1,13 @@
+import "./Friend.css"
+const Friend = ({ friend }) => {
+  console.log(friend);
+  const { email, id , name, phone  } = friend;
 
-
-const Friend = ({friend}) => {
-    console.log(friend)
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div className="friend">
+    <h3>{name}</h3>
+    <p>Email:{email}</p>
+    <p>Phone:{phone}</p>
+  </div>;
 };
 
 export default Friend;
